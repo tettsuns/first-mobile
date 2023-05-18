9456aa81a5d333916f939e4535b4d622fc531e42
Alex <stellmarzie@gmail.com>
2023-05-18 08:10:33 +0800

    added splash image

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#FBD1A2',
     alignItems: 'center',
     justifyContent: 'center',
   },
