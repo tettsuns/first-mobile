1d559868d320862021053ad24f0655e6a9cf87f4
Alex <stellmarzie@gmail.com>
2023-05-26 01:53:10 +0800

    modified final first screen UI

--- App.js
+++ App.js
@@ -1,74 +1,100 @@
 import React from "react";
-import {View, StyleSheet, Image, Text } from "react-native";
+import { StyleSheet, View, Text, Image, Dimensions } from "react-native";
 import { Button } from "@react-native-material/core";
 
-const App = () => (
-  <View style={styles.container}>
-    <View style={styles.background} />
-    <View style={styles.content}>
-      <Image source={require("./assets/logo.png")} style={styles.logo} />
-      <Text style={styles.text}>EcoRide</Text>
-      <View style={styles.buttonContainer}>
-        <Button
-          mode="contained"
-          style={styles.button}
-          onPress={() => console.log("Login pressed")}
-        >
-          Login
-        </Button>
-        <Button
-          mode="contained"
-          style={styles.button}
-          onPress={() => console.log("Register pressed")}
-        >
-          Register
-        </Button>
-      </View>
-    </View>
-  </View>
-);
+const windowWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "#B1E19C",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingHorizontal: 20,
+    paddingBottom: 20,
   },
-  background: {
+  backgroundCircle: {
     position: "absolute",
     bottom: 0,
-    left: -200,
-    right: -200,
-    height: 400,
-    borderRadius: 400,
-    backgroundColor: "#F2F2F2",
-  },
-  content: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 20,
+    width: windowWidth * 1.1,
+    height: windowWidth,
+    borderRadius: windowWidth / 2,
+    backgroundColor: "#61AF82",
+    transform: [{ translateY: windowWidth / 2.8 }],
+    zIndex: -1,
   },
   logo: {
-    width: 150,
-    height: 150,
+    width: 250,
+    height: 250,
     marginBottom: 20,
+    marginTop: 100,
   },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
+  slogan: {
+    fontSize: 20,
     marginBottom: 40,
-    color: "white",
+    color: "#118540",
     textAlign: "center",
   },
   buttonContainer: {
     width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  buttonWrapper: {
+    width: "85%",
+    borderRadius: 35,
+    overflow: "hidden",
+    marginBottom: 30,
+    justifyContent: "center",
   },
   button: {
-    width: "48%",
-    marginBottom: 20,
+    height: 75,
+    backgroundColor: "#118540",
+    color: "#FFFFFF",
+    padding: 18,
+  },
+  buttonText: {
+    fontSize: 22,
+    textAlign: "center",
+  },
+  copyrightText: {
+    fontSize: 12,
+    color: "#118540",
+    textAlign: "center",
+    marginTop: "auto",
   },
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require("./assets/logo.png")} style={styles.logo} />
+      <Text style={styles.slogan}>RIDE TOGETHER, SAVE THE PLANET!</Text>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonWrapper}>
+          <Button
+            title="LOGIN"
+            mode="contained"
+            onPress={() => alert("Login button clicked!")}
+            style={styles.button}
+            titleStyle={styles.buttonText}
+          />
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button
+            title="REGISTER"
+            mode="contained"
+            onPress={() => alert("Register button clicked!")}
+            style={styles.button}
+            titleStyle={styles.buttonText}
+          />
+        </View>
+      </View>
+      <View style={styles.backgroundCircle} />
+      <Text style={styles.copyrightText}>
+        &copy; {new Date().getFullYear()} EcoRide
+      </Text>
+    </View>
+  );
+};
 
 export default App;
